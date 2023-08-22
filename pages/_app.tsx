b78e703390303ab4b823fef02f9d0cd97ad480e1
Reno Davids <reno.davids@sovtech.com>
2023-08-22 16:04:54 +0200

    Champions page with champion card

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,35 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "../styles/ChampionCard.css";
+
+const customTheme = extendTheme({
+  styles:{
+      global:{
+          body:{
+              bg: "#010A13"
+          }
+      },
+  },
+  fonts:{
+      body: "'BeaufortforLOL'"
+  },
+  fontSizes: {
+      "7xl": "5rem"
+  }, 
+  breakpoints:{
+      mobile: "375px", 
+      tablet: "750px", 
+      monitor: "1024px"
+  }
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={customTheme}>
+      <Box m={5}>
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
-  )
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,52 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Button } from '@chakra-ui/react'
+import type { NextPage } from "next";
+import {SimpleGrid } from "@chakra-ui/react";
+import { Champion } from "../types/Champion";
+import ChampionCard from "../components/ChampionCard";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ champions }) => {
   return (
-    <Button bg="red">Test</Button>
-  )
-}
+    <SimpleGrid gap={6} columns={5}>
+      {champions.map((champion: Champion) => (
+        <ChampionCard
+          id={champion.id}
+          name={champion.id}
+          tags={champion.tags}
+          title={champion.title}
+          info={champion.info}
+          key={champion.id}
+        />
+      ))}
+    </SimpleGrid>
+  );
+};
 
-export default Home
+export const getStaticProps = async () => {
+  const response = await fetch(
+    "https://ddragon.leagueoflegends.com/cdn/13.7.1/data/en_US/champion.json"
+  );
+
+  const data = await response.json();
+
+  let champions: Champion[] = [];
+
+  Object.values(data.data).forEach((champion: any) => {
+    champions.push({
+      id: champion.id,
+      title: champion.title,
+      name: champion.name,
+      tags: champion.tags,
+      info: {
+        attack: champion.info.attack,
+        defense: champion.info.defense,
+        magic: champion.info.magic,
+        difficulty: champion.info.difficulty,
+      },
+    });
+  });
+
+  return {
+    props: { champions },
+  };
+};
+
+export default Home;
