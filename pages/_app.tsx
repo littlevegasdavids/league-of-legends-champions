ccf72b488ebe5a21e1203267532fba1f4bdd871a
Reno Davids <reno.davids@sovtech.com>
2023-08-22 16:13:05 +0200

    Added league website icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "../styles/ChampionCard.css";
 import '../styles/ChampionPage.css'
 import '../styles/font.css'
+import Head from "next/head";
 
 const customTheme = extendTheme({
   styles:{
@@ -27,6 +28,9 @@ const customTheme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={customTheme}>
+      <Head>
+        <link rel="icon" href="leagueIcon.png"/>
+      </Head>
       <Box m={5}>
         <Component {...pageProps} />
       </Box>
